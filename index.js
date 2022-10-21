728ef1b4bd026a1d088ab1225fe48223fad8a4fe
yousif <yousifr.ismail@gmail.com>
2022-10-22 00:26:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function logShout(string){
     console.log(string)
 }
 logShout("HELLO")
+
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function introductionWithLanguage (name,language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function sayHelloTo(firstName = "User") {
+  console.log(`Hello, ${firstName}!`);
+}
+
+function introductionWithLanguage (name,language="javascript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
